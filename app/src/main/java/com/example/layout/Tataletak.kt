8896e67ec99d1cfd28d9779b5eaaacbecfd4a286
syairaarif <putrisyafira4444@gmail.com>
2025-10-17 10:43:17 +0700

    menambahkan modifier agar peletakan lebih rapih

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -34,12 +34,16 @@ fun TataletakColumn(modifier: Modifier) {
 }
 
 @Composable
-fun TataletakRow() {
-    Text(text = "Komponen1")
-    Text(text = "Komponen2")
-    Text(text = "Komponen3")
-    Text(text = "Komponen4")
+fun TataletakRow(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
 
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
 }
 
 
